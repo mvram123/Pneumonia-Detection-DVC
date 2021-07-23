c7ce4106a351cc28f44426a36ddc0ee714b2464c
mvram123 <mramarao923@gmail.com>
2021-07-23 09:47:15 +0530

    model and model summary saved

--- src/training.py
+++ src/training.py
@@ -1,9 +1,24 @@
+import os
 import time
+from datetime import datetime
 import argparse
+from tensorflow import keras
 from tensorflow.keras.layers import Input, Lambda, Dense, Flatten
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications.vgg16 import VGG16
 from data_preprocessing import read_params, pre_processing
+
+
+class TimeHistory(keras.callbacks.Callback):
+
+    def on_train_begin(self, logs={}):
+        self.times = []
+
+    def on_epoch_begin(self, epoch, logs={}):
+        self.epoch_time_start = time.time()
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.times.append(time.time() - self.epoch_time_start)
 
 
 def training_model(config_path):
@@ -17,6 +32,11 @@ def training_model(config_path):
     loss = config['estimators']['VGG16']['params']['loss']
     optimizer = config['estimators']['VGG16']['params']['optimizer']
     metrics = config['estimators']['VGG16']['params']['metrics']
+
+    model_summary_path = config['estimators']['store']['model_summary_path']
+    metrics_path = config['estimators']['store']['metrics_path']
+
+    model_dir = config['model_dir']
 
     output = config['base']['output']
 
@@ -41,15 +61,31 @@ def training_model(config_path):
         metrics=[metrics]
     )
 
+    # Current datetime
+    dt_string = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+    file_name = 'model_sum_'+ dt_string + '.txt'
+
+    # Saving Model Summary in a file
+    with open(os.path.join(model_summary_path, file_name), 'w') as f:
+        model.summary(print_fn=lambda a: f.write(a + '\n'))
+
     training_set, test_set = pre_processing(config_path=config_path)
 
+    time_callback = TimeHistory()
     history = model.fit(training_set,
                         validation_data=test_set,
                         epochs=1,
+                        callbacks=[time_callback],
                         steps_per_epoch=len(training_set),
                         validation_steps=len(test_set))
 
+    time_taken_for_execution = sum(time_callback.times)
 
+    print(f'time taken : {time_taken_for_execution}')
+
+    # Saving Model
+    model_name = 'model_' + dt_string + '.h5'
+    model.save(os.path.join(model_dir, model_name))
 
 
 if __name__ == "__main__":
