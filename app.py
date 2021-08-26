361ec80106b8752b649c72027ccf696651699cd3
mvram123 <mramarao923@gmail.com>
2021-08-26 20:20:09 +0530

    change to flask

--- app.py
+++ app.py
@@ -1,47 +1,48 @@
-import tensorflow as tf
-import numpy as np
-import streamlit as st
-from PIL import Image
-import requests
-from io import BytesIO
+from __future__ import division, print_function
+import os
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+from prediction import prediction
 
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+# Reading Model Path
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.title("Pnuemonia Detection Image Classifier")
-st.text("Provide URL of Chest Xray for Pneumonia Detection")
+MODEL_PATH = "models_saved/model.h5"
 
-
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model('models_saved/model.h5')
-    return model
+# Define a flask app
+app = Flask(__name__)
 
 
-with st.spinner('Loading Model Into Memory....'):
-  model = load_model()
-
-classes = ['Pneumonia', 'Healthy']
-
-
-def decode_img(image):
-    img = tf.image.decode_jpeg(image, channels=3)
-    img = tf.image.resize(img, [224, 224])
-    return np.expand_dims(img, axis=0)
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
 
 
-path = st.text_input('Enter Image URL to Classify.. ',
-                     'https://raw.githubusercontent.com/mvram123/mvram123/main/p4.jpeg')
-if path is not None:
-    content = requests.get(path).content
+@app.route('/predict', methods=['POST'])
+def upload():
+    # Get the file from post request
 
-    st.write("Predicted Class :")
-    with st.spinner('classifying.....'):
-        label = np.argmax(model.predict(decode_img(content)), axis=1)
-    print(label)
-    st.write(classes[label[0]])
-    st.write("")
-    image = Image.open(BytesIO(content))
-    st.image(image, caption='Pneumonia Detection', use_column_width=True)
+    f = request.files['file']
+    print(f.filename)
 
+    # Save the file to ./uploads
+
+    base_path = os.path.dirname(__file__)
+    print("base path obtained")
+
+    if 'n' in f.filename:
+        file_path = os.path.join(base_path, 'data/test/NORMAL', secure_filename(f.filename))
+    else:
+        file_path = os.path.join(base_path, 'data/test/PNEUMONIA', secure_filename(f.filename))
+
+    print(file_path)
+    print('Model Prediction started')
+
+    # Make prediction
+    output = prediction(img_path=file_path, model_path=MODEL_PATH)
+
+    return output
+
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
